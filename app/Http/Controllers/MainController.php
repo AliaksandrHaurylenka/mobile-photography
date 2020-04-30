10aa6dac762a1b4f1005c0d94927c9ceba3cc2e9
Aliaksandr Haurylenka <aliaksandr.haurylenka@gmail.com>
2020-04-30 15:51:22 +0300

    asd

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,11 +7,14 @@ use App\Section;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 
+use App\Program;
+
 class MainController extends Controller
 {
     public function index()
     {
+        $program = Program::all();
 
-        return view('site.index');
+        return view('site.index', compact('program'));
     }
 }
