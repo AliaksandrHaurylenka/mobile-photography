d4b21c02626caa4ffe64865311b9e2377bea5f1a
AliaksandrHaurylenka <goric0312@mail.ru>
2020-05-12 09:38:54 +0300

    asd

--- resources/views/admin/portfolios/edit.blade.php
+++ resources/views/admin/portfolios/edit.blade.php
@@ -13,7 +13,7 @@
         <div class="panel-body">
             <div class="row">
                 <div class="col-xs-12 form-group">
-                    {!! Form::label('photo', trans('quickadmin.portfolio.fields.photo').'*', ['class' => 'control-label']) !!}
+                    {!! Form::label('photo', trans('quickadmin.portfolio.fields.photo'), ['class' => 'control-label']) !!}
                     {!! Form::hidden('photo', old('photo')) !!}
                     {!! Form::file('photo', ['class' => 'form-control']) !!}
                     {!! Form::hidden('photo_max_size', 2) !!}
@@ -28,7 +28,7 @@
 
             <div class="row">
                 <div class="col-xs-12 form-group">
-                    {!! Form::label('photo_after', trans('quickadmin.portfolio.fields.photo_after').'*', ['class' => 'control-label']) !!}
+                    {!! Form::label('photo_after', trans('quickadmin.portfolio.fields.photo_after'), ['class' => 'control-label']) !!}
                     {!! Form::hidden('photo_after', old('photo_after')) !!}
                     {!! Form::file('photo_after', ['class' => 'form-control']) !!}
                     {!! Form::hidden('photo_after_max_size', 2) !!}
@@ -43,7 +43,7 @@
 
             <div class="row">
                 <div class="col-xs-12 form-group">
-                    {!! Form::label('category_id', trans('quickadmin.portfolio.fields.category').'*', ['class' => 'control-label']) !!}
+                    {!! Form::label('category_id', trans('quickadmin.portfolio.fields.category'), ['class' => 'control-label']) !!}
                     {!! Form::select('category_id', $categories, old('category_id'), ['class' => 'form-control select2', 'required' => '']) !!}
                     <p class="help-block"></p>
                     @if($errors->has('category_id'))
