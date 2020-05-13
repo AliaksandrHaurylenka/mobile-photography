d3a84c9049f2180d190faffc466c1db2de5da755
AliaksandrHaurylenka <goric0312@mail.ru>
2020-05-13 08:58:35 +0300

    asd

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-05-12 17:37:15.
+    * Generated on 2020-05-13 05:27:15.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
